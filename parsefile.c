ccc1f5186ae6b4c6589d4b471acb72157282c71a
Oliver Hookins <ohookins@gmail.com>
2022-06-19 22:57:45 +1000

    Complete file reading.

--- parsefile.c
+++ parsefile.c
@@ -3,6 +3,9 @@
 #include <sys/errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 #include "parsefile.h"
 
@@ -12,6 +15,7 @@ void parsefile(const char *filename)
     int fd = openfile(filename);
 
     // mmap the file descriptor
+    void *map = mmapfile(fd);
 
     // start parsing the memory range for mp4 boxes
 }
@@ -24,8 +28,46 @@ int openfile(const char *filename)
     if (fd < 0)
     {
         printf("Error %d opening file: %s\n", errno, strerror(errno));
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     return fd;
+}
+
+void *mmapfile(const int fd)
+{
+    // We need to know the file size in order to figure out how much memory to
+    // map, otherwise we'll get EINVAL.
+    int filesize = getfilesize(fd);
+
+    // Probably won't be daring enough to make changes in place,
+    // so this should stay PROT_READ for now. Actually would be a better
+    // idea to write out a duplicate file with correcions rather than
+    // edit in place anyway...
+    void *map = mmap(NULL, (size_t)filesize, PROT_READ, MAP_FILE | MAP_PRIVATE, fd, 0);
+
+    if (map == MAP_FAILED)
+    {
+        printf("Error %d mmaping file: %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    return map;
+}
+
+int getfilesize(const int fd)
+{
+    struct stat st;
+
+    int err = fstat(fd, &st);
+    if (err < 0)
+    {
+        printf("Error %d getting file size: %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    int filesize = (int)st.st_size;
+
+    printf("File size: %d\n", filesize);
+    return filesize;
 }
--- parsefile.h
+++ parsefile.h
@@ -1,3 +1,7 @@
 void parsefile(const char *filename);
 
 int openfile(const char *filename);
+
+void *mmapfile(const int fd);
+
+int getfilesize(const int fd);
