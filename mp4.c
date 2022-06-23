f2a0fa4db22b3d5f1e74084ff2438ccc47c7192b
Oliver Hookins <ohookins@gmail.com>
2022-06-23 23:15:00 +1000

    Turn box headers into structs and memcpy them as one operation.

--- mp4.c
+++ mp4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <stdlib.h>
 
 #include "mp4.h"
 
@@ -18,9 +19,6 @@ void decode_mp4(const void *map, const int length)
         // Nested boxes are handled by recursing.
         int parsed_bytes = decode_box(current);
         current += parsed_bytes;
-
-        // determine whether we need to recurse into this box or not
-        // TODO
     }
 
     printf("\nFinished reading file\n");
@@ -28,162 +26,71 @@ void decode_mp4(const void *map, const int length)
 
 int decode_box(void *map)
 {
-    // it's mainly useful to have a structure to store the base metadata in due to
-    // the union and optional fields.
-    // default to compact size notation, although we'll probably never see extended anyway.
-    struct Box box;
-    box.size_flag = COMPACT;
-
-    // get the box size and type and delegate further parsing to the parser for the
-    // particular box type
-    u_int32_t boxsize;
-    memcpy(&boxsize, map, sizeof(boxsize));
-    map += sizeof(boxsize);
+    struct BaseBox box;
+    memcpy(&box, map, sizeof(box));
+    map += sizeof(box);
 
     // We can't actually read numbers out of the file/map properly due to endian-ness,
-    // so we fix it here. Yes this is horrible. I am committing many C sins.
-    boxsize = htonl(boxsize);
-
-    // get the box type which is in the base box class
-    memcpy(&box.type, (const char *)map, sizeof(box.type));
-    map += sizeof(box.type);
+    // so we fix it here.
+    box.size = htonl(box.size);
 
     // Next field is optionally largesize, if the first field was set to 1.
     // in practice, I'm not bothering to check for this anywhere else, so it
     // should probably be removed.
-    if (boxsize == 1)
+    if (box.size == 1)
     {
-        box.size_flag = EXTENDED;
-        memcpy(&box.size.extended, map, sizeof(box.size.extended));
-        box.size.extended = htonll(box.size.extended);
-        map += sizeof(box.size.extended);
-    }
-    else
-    {
-        box.size.compact = boxsize;
+        printf("Extended box size not supported.\n");
+        exit(EXIT_FAILURE);
     }
 
     // Parse each box type based on the type field we parsed. There's probably a
     // much better way of doing this with some kind of function pointer table.
-    if (strcmp(box.type, "ftyp") == 0)
+    if (strncmp(box.type, "ftyp", sizeof(box.type)) == 0)
     {
         decode_ftyp(map, box);
     }
-    else if (strcmp(box.type, "mdat") == 0)
+    else if (strncmp(box.type, "mdat", sizeof(box.type)) == 0)
     {
         decode_mdat(map, box);
     }
-    else if (strcmp(box.type, "moov") == 0)
+    else if (strncmp(box.type, "moov", sizeof(box.type)) == 0)
     {
         decode_nested_box(map, box);
     }
-    else if (strcmp(box.type, "mvhd") == 0)
+    else if (strncmp(box.type, "mvhd", sizeof(box.type)) == 0)
     {
         decode_mvhd(map, box);
     }
-    else if (strcmp(box.type, "udta") == 0)
+    else if (strncmp(box.type, "udta", sizeof(box.type)) == 0)
     {
         decode_nested_box(map, box);
     }
-    else if (strcmp(box.type, "trak") == 0)
+    else if (strncmp(box.type, "trak", sizeof(box.type)) == 0)
     {
         decode_nested_box(map, box);
+    }
+    else if (strncmp(box.type, "tkhd", sizeof(box.type)) == 0)
+    {
+        decode_tkhd(map, box);
     }
     else
     {
-        printf("Unknown box type: [%s]\n", box.type);
+        printf("%*s[%.4s] size [%d] - unknown box type\n", mp4NestingLevel, "", box.type, box.size);
     }
 
     // This box continues until the end of the file, so return.
     // We'll decode inner boxes recursively.
-    if (boxsize == 0)
+    if (box.size == 0)
     {
         return 0;
     }
 
-    return boxsize;
+    return box.size;
 }
 
-void decode_ftyp(void *map, const struct Box box)
+void decode_nested_box(void *map, const struct BaseBox box)
 {
-    // get the major brand
-    char majorBrand[4];
-    char minorVersion[4];
-    char additionalBrands[4];
-
-    memcpy(&majorBrand, (const char *)map, sizeof(majorBrand));
-    map += sizeof(majorBrand);
-    memcpy(&minorVersion, (const char *)map, sizeof(minorVersion));
-    map += sizeof(minorVersion);
-
-    printf("%*s[ftyp] size [%u] major brand [%.4s] minor version [%.4s] additional brands [", mp4NestingLevel, "", box.size.compact, majorBrand, minorVersion);
-
-    // Additional brands are from here until the end of the box.
-    // Have to remove 8 for the size field itself and the type field.
-    u_int32_t remainingLength = box.size.compact - 8 - sizeof(majorBrand) - sizeof(minorVersion);
-
-    while (remainingLength > 0)
-    {
-        memcpy(&additionalBrands, (const char *)map, sizeof(additionalBrands));
-        map += sizeof(additionalBrands);
-        remainingLength -= sizeof(additionalBrands);
-        printf("%.4s,", additionalBrands);
-    }
-    printf("\b]\n");
-}
-
-void decode_mdat(void *map, const struct Box box)
-{
-    // not much to this box, it's just audio/video data
-    printf("%*s[mdat] size [%u]\n", mp4NestingLevel, "", box.size.compact);
-}
-
-void decode_mvhd(void *map, const struct Box box)
-{
-    int version;
-    memcpy(&version, map, sizeof(version));
-    map += sizeof(version);
-
-    // couldn't be bothered supporting version 1
-    if (version != 0)
-    {
-        printf("%*s[mvdh] size [%u] version [%d]\n", mp4NestingLevel, "", box.size.compact, version);
-        return;
-    }
-
-    u_int32_t creation_time;
-    memcpy(&creation_time, map, sizeof(creation_time));
-    map += sizeof(creation_time);
-
-    u_int32_t mod_time;
-    memcpy(&mod_time, map, sizeof(mod_time));
-    map += sizeof(mod_time);
-
-    uint32_t timescale;
-    memcpy(&timescale, map, sizeof(timescale));
-    map += sizeof(timescale);
-
-    uint32_t duration;
-    memcpy(&duration, map, sizeof(duration));
-    map += sizeof(duration);
-
-    int32_t rate;
-    memcpy(&rate, map, sizeof(rate));
-    map += sizeof(rate);
-
-    int16_t volume;
-    memcpy(&volume, map, sizeof(volume));
-    map += sizeof(volume);
-
-    // there are more fields, but they aren't very interesting
-
-    printf("%*s[mvdh] size [%u] version [%d] creation [%d] modified [%d] timescale [%d] duration [%d] rate [%#x] volume [%#x]\n",
-           mp4NestingLevel, "", box.size.compact, version, creation_time, mod_time, timescale, duration, rate, volume);
-}
-
-void decode_nested_box(void *map, const struct Box box)
-{
-    printf("%*s[%s] size [%u]\n", mp4NestingLevel, "", box.type, box.size.compact);
+    printf("%*s[%.4s] size [%u]\n", mp4NestingLevel, "", box.type, box.size);
 
     // this box is nested
     mp4NestingLevel += 2;
@@ -194,7 +101,7 @@ void decode_nested_box(void *map, const struct Box box)
     // the end of the box is the current position + the size, minus the size of the
     // size field and the size of the type field, which have already been read
     // in the calling function, and are included in the overall box size
-    void *end_of_box = map + box.size.compact - sizeof(box.type) - sizeof(box.size.compact);
+    void *end_of_box = map + box.size - sizeof(box.type) - sizeof(box.size);
 
     while (current < end_of_box)
     {
@@ -203,4 +110,89 @@ void decode_nested_box(void *map, const struct Box box)
     }
 
     mp4NestingLevel -= 2;
+}
+
+void decode_ftyp(void *map, const struct BaseBox box)
+{
+    // get the major brand & minor version
+    struct FtypBox ftypBox;
+    memcpy(&ftypBox, (const char *)map, sizeof(ftypBox));
+    map += sizeof(ftypBox);
+
+    printf("%*s[ftyp] size [%u] major brand [%.4s] minor version [%.4s] additional brands [", mp4NestingLevel, "", box.size, ftypBox.majorBrand, ftypBox.minorVersion);
+
+    // Additional brands are from here until the end of the box.
+    // Have to remove 8 for the size field itself and the type field.
+    u_int32_t remainingLength = box.size - 8 - sizeof(ftypBox);
+
+    char additionalBrands[4];
+    while (remainingLength > 0)
+    {
+        memcpy(&additionalBrands, (const char *)map, sizeof(additionalBrands));
+        map += sizeof(additionalBrands);
+        remainingLength -= sizeof(additionalBrands);
+        printf("%.4s,", additionalBrands);
+    }
+    printf("\b]\n");
+}
+
+void decode_mdat(void *map, const struct BaseBox box)
+{
+    // not much to this box, it's just audio/video data
+    printf("%*s[mdat] size [%u]\n", mp4NestingLevel, "", box.size);
+}
+
+void decode_mvhd(void *map, const struct BaseBox box)
+{
+    struct MvhdBox mvhdBox;
+    memcpy(&mvhdBox, map, sizeof(mvhdBox));
+    map += sizeof(mvhdBox);
+
+    // fix the number endianness
+    mvhdBox.creation_time = htonl(mvhdBox.creation_time);
+    mvhdBox.mod_time = htonl(mvhdBox.mod_time);
+    mvhdBox.timescale = htonl(mvhdBox.timescale);
+    mvhdBox.duration = htonl(mvhdBox.duration);
+    mvhdBox.rate = htonl(mvhdBox.rate);
+    mvhdBox.volume = htons(mvhdBox.volume);
+
+    // there are more fields, but they aren't very interesting
+
+    // couldn't be bothered supporting version 1
+    if (mvhdBox.version != 0)
+    {
+        printf("%*s[mvdh] size [%u] version [%u]\n", mp4NestingLevel, "", box.size, mvhdBox.version);
+        return;
+    }
+
+    printf("%*s[mvdh] size [%u] version [%u] flags [%#x] creation [%u] modified [%u] timescale [%u] duration [%u] rate [%#x] volume [%#x]\n",
+           mp4NestingLevel, "", box.size, mvhdBox.version, mvhdBox.flags, mvhdBox.creation_time, mvhdBox.mod_time, mvhdBox.timescale,
+           mvhdBox.duration, mvhdBox.rate, mvhdBox.volume);
+}
+
+void decode_tkhd(void *map, const struct BaseBox box)
+{
+    struct TkhdBox tkhdBox;
+    memcpy(&tkhdBox, map, sizeof(tkhdBox));
+    map += sizeof(tkhdBox);
+
+    // fix the number endianness
+    tkhdBox.version = htonl(tkhdBox.version);
+    tkhdBox.creation_time = htonl(tkhdBox.creation_time);
+    tkhdBox.mod_time = htonl(tkhdBox.mod_time);
+    tkhdBox.track_id = htonl(tkhdBox.track_id);
+    tkhdBox.duration = htonl(tkhdBox.duration);
+    tkhdBox.width = htonl(tkhdBox.width);
+    tkhdBox.height = htonl(tkhdBox.height);
+
+    // couldn't be bothered supporting version 1
+    if (tkhdBox.version == 1)
+    {
+        printf("%*s[tkhd] size [%u] version [%u]\n", mp4NestingLevel, "", box.size, tkhdBox.version);
+        return;
+    }
+
+    printf("%*s[tkhd] size [%u] version [%u] flags [%#x] creation [%u] modified [%u] track [%u] duration [%u] width [%u] height [%u]\n",
+           mp4NestingLevel, "", box.size, tkhdBox.version, tkhdBox.flags, tkhdBox.creation_time, tkhdBox.mod_time, tkhdBox.track_id,
+           tkhdBox.duration, tkhdBox.width, tkhdBox.height);
 }
--- mp4.h
+++ mp4.h
@@ -1,21 +1,51 @@
 #include <stdbool.h>
 
-enum mp4_size_flag
+// Struct definitions for the box types I'm interested in.
+// I'm not bothering with extended size boxes or additional fields, to keep
+// things simple. I'm also not defining any box types that aren't useful for the
+// media files I have on hand.
+struct BaseBox
 {
-    COMPACT,
-    EXTENDED,
+    u_int32_t size;
+    char type[4];
 };
 
-struct Box
+// ftyp always has a variable length, these are the only two guaranteed fields
+struct FtypBox
 {
-    char type[4];
-    enum mp4_size_flag size_flag;
-    union
-    {
-        u_int32_t compact;
-        u_int64_t extended;
-    } size;
-    u_int8_t extended_type[16];
+    char majorBrand[4];
+    char minorVersion[4];
+};
+
+struct MvhdBox
+{
+    u_int8_t version;
+    unsigned int flags : 24;
+    u_int32_t creation_time;
+    u_int32_t mod_time;
+    u_int32_t timescale;
+    u_int32_t duration;
+    int32_t rate;
+    int16_t volume;
+};
+
+struct TkhdBox
+{
+    u_int8_t version;
+    unsigned int flags : 24;
+    u_int32_t creation_time;
+    u_int32_t mod_time;
+    u_int32_t track_id;
+    u_int32_t reserved1;
+    u_int32_t duration;
+    int32_t reserved2[2];
+    int16_t layer;
+    int16_t alternate_group;
+    int16_t volume;
+    u_int16_t reserved3;
+    int32_t unity_matrix[9];
+    u_int32_t width;
+    u_int32_t height;
 };
 
 // decode_mp4 starts the overall process of decoding the MP4 file.
@@ -29,12 +59,17 @@ int decode_box(void *map);
 
 // it turns out that boxes that are just containers for other boxes all look the
 // same, so we can just use the same function for them
-void decode_nested_box(void *map, const struct Box box);
+void decode_nested_box(void *map, const struct BaseBox box);
 
 // Following functions handle the different kind of mp4 boxes that might be found.
 // These are "leaf" boxes without further nesting.
-void decode_ftyp(void *map, const struct Box box);
+void decode_ftyp(void *map, const struct BaseBox box);
 
-void decode_mdat(void *map, const struct Box box);
+void decode_mdat(void *map, const struct BaseBox box);
 
-void decode_mvhd(void *map, const struct Box box);
+void decode_mvhd(void *map, const struct BaseBox box);
+
+void decode_tkhd(void *map, const struct BaseBox box);
+
+// decode a timestamp - these are in seconds since midnight, Jan 1, 1904
+// so convenient!
