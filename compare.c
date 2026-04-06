c2691333846ddcbfce0b25cdaaa3c4cccaef9538
Oliver Hookins <ohookins@gmail.com>
2026-04-06 11:54:31 +1000

    Print missing sub-boxes.

--- compare.c
+++ compare.c
@@ -7,20 +7,34 @@
 #include "mp4.h"
 
 
-#define MAX_BOXES 32
-typedef struct { char type[4]; } BoxType;
 
-// Helper: enumerate top-level boxes and store their types
-static int collect_box_types(const void *map, int filesize, BoxType *out_types, int max_types) {
+#define MAX_BOXES 256
+typedef struct { char path[128]; char type[4]; } BoxPath;
+
+// Recursively collect all boxes (with hierarchy path)
+static int collect_boxes(const void *map, int filesize, BoxPath *out_boxes, int max_boxes, const char *parent_path) {
     int count = 0;
     void *current = (void *)map;
     void *end = (void *)(map + filesize);
-    while (current < end && count < max_types) {
+    while (current < end && count < max_boxes) {
         struct BaseBox box;
         memcpy(&box, current, sizeof(box));
         uint32_t size = ntohl(box.size);
-        memcpy(out_types[count].type, box.type, 4);
+        char path[128];
+        if (parent_path && parent_path[0])
+            snprintf(path, sizeof(path), "%s/%.4s", parent_path, box.type);
+        else
+            snprintf(path, sizeof(path), "%.4s", box.type);
+        memcpy(out_boxes[count].path, path, sizeof(out_boxes[count].path));
+        memcpy(out_boxes[count].type, box.type, 4);
         count++;
+        // If this is a known container box, recurse
+        if (strncmp(box.type, "moov", 4) == 0 || strncmp(box.type, "trak", 4) == 0 ||
+            strncmp(box.type, "mdia", 4) == 0 || strncmp(box.type, "minf", 4) == 0 ||
+            strncmp(box.type, "stbl", 4) == 0 || strncmp(box.type, "udta", 4) == 0) {
+            int inner = collect_boxes(current + 8, size - 8, out_boxes + count, max_boxes - count, path);
+            count += inner;
+        }
         if (size == 0 || size > (end - current)) break;
         current += size;
     }
@@ -33,31 +47,32 @@ void analyze_missing_boxes(const char *target_filename, const char *reference_fi
     void *target_map = map_file(target_filename, &target_size, &target_fd);
     void *ref_map = map_file(reference_filename, &ref_size, &ref_fd);
 
-    BoxType target_boxes[MAX_BOXES], ref_boxes[MAX_BOXES];
-    int target_count = collect_box_types(target_map, target_size, target_boxes, MAX_BOXES);
-    int ref_count = collect_box_types(ref_map, ref_size, ref_boxes, MAX_BOXES);
 
-    printf("Top-level boxes in %s:\n", target_filename);
+    BoxPath target_boxes[MAX_BOXES], ref_boxes[MAX_BOXES];
+    int target_count = collect_boxes(target_map, target_size, target_boxes, MAX_BOXES, "");
+    int ref_count = collect_boxes(ref_map, ref_size, ref_boxes, MAX_BOXES, "");
+
+    printf("All boxes in %s:\n", target_filename);
     for (int i = 0; i < target_count; ++i) {
-        printf("  %.4s\n", target_boxes[i].type);
+        printf("  %s\n", target_boxes[i].path);
     }
-    printf("Top-level boxes in %s:\n", reference_filename);
+    printf("All boxes in %s:\n", reference_filename);
     for (int i = 0; i < ref_count; ++i) {
-        printf("  %.4s\n", ref_boxes[i].type);
+        printf("  %s\n", ref_boxes[i].path);
     }
 
-    // Compare and print missing boxes
+    // Compare and print missing boxes (by path)
     printf("\nBoxes present in %s but missing from %s:\n", target_filename, reference_filename);
     for (int i = 0; i < target_count; ++i) {
         int found = 0;
         for (int j = 0; j < ref_count; ++j) {
-            if (memcmp(target_boxes[i].type, ref_boxes[j].type, 4) == 0) {
+            if (strcmp(target_boxes[i].path, ref_boxes[j].path) == 0) {
                 found = 1;
                 break;
             }
         }
         if (!found) {
-            printf("  %.4s\n", target_boxes[i].type);
+            printf("  %s\n", target_boxes[i].path);
         }
     }
 
